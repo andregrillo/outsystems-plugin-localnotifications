f4f88c2904e97a3ce949bc55db2b302a42f79eb0
André Grillo <andregrillo@gmail.com>
2025-05-29 15:08:11 +0100

    callback implemented

--- src/ios/LocalNotifications.swift
+++ src/ios/LocalNotifications.swift
@@ -2,8 +2,17 @@ import Foundation
 
 @objc(LocalNotifications)
 class LocalNotifications: CDVPlugin {
+    
     @objc(coolMethod:)
     func coolMethod(command: CDVInvokedUrlCommand) {
-         print("Cool method called")
+        print("Cool method called")
+        
+        sendPluginResult(status: .ok, message: "Everything went ok", callbackId: command.callbackId)
+    }
+    
+    func sendPluginResult(status: CDVCommandStatus, message: String = "", callbackId: String, keepCallback: Bool = false) {
+        let pluginResult = CDVPluginResult(status: status, messageAs: message)
+        pluginResult?.setKeepCallbackAs(keepCallback)
+        self.commandDelegate!.send(pluginResult, callbackId: callbackId)
     }
 }
