8ab3cde8669461539a00c31cca5227dcfc60231b
André Grillo <andregrillo@gmail.com>
2025-06-16 11:40:44 +0100

    checkPermissions updated

--- src/ios/LocalNotifications.swift
+++ src/ios/LocalNotifications.swift
@@ -1,4 +1,5 @@
 import Foundation
+import UserNotifications
 
 @objc(LocalNotifications)
 class LocalNotifications: CDVPlugin {
@@ -7,7 +8,25 @@ class LocalNotifications: CDVPlugin {
     func setLocalNotifications(command: CDVInvokedUrlCommand) {
         print("Cool method called")
         
-        sendPluginResult(status: .ok, message: "Everything went ok", callbackId: command.callbackId)
+        sendPluginResult(status: .ok, message: "All good", callbackId: command.callbackId)
+    }
+    
+    @objc(checkPermissions:)
+    func checkPermissions(command: CDVInvokedUrlCommand) {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+            if error != nil {
+                print(error!.localizedDescription)
+                self.sendPluginResult(status: .error, message: error!.localizedDescription, callbackId: command.callbackId)
+            } else {
+                if success {
+                    print("All set!")
+                    self.sendPluginResult(status: .ok, message: "Permission IS allowed!", callbackId: command.callbackId)
+                } else {
+                    print("Permission not allowed!")
+                    self.sendPluginResult(status: .ok, message: "Permission NOT allowed!", callbackId: command.callbackId)
+                }
+            }
+        }
     }
     
     func sendPluginResult(status: CDVCommandStatus, message: String = "", callbackId: String, keepCallback: Bool = false) {
--- www/localnotifications.js
+++ www/localnotifications.js
@@ -3,3 +3,7 @@ var exec = require('cordova/exec');
 exports.setLocalNotifications = function (success, error) {
     exec(success, error, 'LocalNotifications', 'setLocalNotifications');
 };
+
+exports.checkPermissions = function (success, error) {
+    exec(success, error, 'LocalNotifications', 'checkPermissions');
+};
